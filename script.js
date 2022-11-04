ad7662882c2bc8976e28e4056bcc470596fb22d3
waleriQ <interceptor55@icloud.com>
2022-11-04 21:21:23 +0600

    entries/fromentries

---
+++ script.js
@@ -0,0 +1,16 @@
+// *entries / fromentries *//
+
+// let a = {  //Object
+//     name: 'Albert',
+//     age: 25,
+//     lastname: 'Ivanov'
+// };
+// let b = [  //Entries
+//     [name, 'albert'],
+//     ['age', 25],
+//     ['lastname', 'Ivanov']
+// ]
+//
+// console.log(Object.entries(a));
+// console.log(Object.fromEntries(b));
+
