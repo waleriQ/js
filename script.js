8da55a6eeb8b35b378a2076de41da849981a16cc
waleriQ <interceptor55@icloud.com>
2022-11-06 00:53:50 +0600

    array

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 // *entries / fromentries *//
 
-// let a = {  //Object
-//     name: 'Albert',
-//     age: 25,
-//     lastname: 'Ivanov'
-// };
-// let b = [  //Entries
-//     [name, 'albert'],
-//     ['age', 25],
-//     ['lastname', 'Ivanov']
-// ]
-//
+let a = {  //Object
+    name: 'Albert',
+    age: 25,
+    lastname: 'Ivanov'
+};
+
+let b = [  //Entries
+    ['name', 'albert'],
+    ['age', 25],
+    ['lastname', 'Ivanov']
+]
+
 // console.log(Object.entries(a));
 // console.log(Object.fromEntries(b));
+// const mapping = new Map(b);
+// mapping.set('gender', 'man');
+// console.log(b);
 
+// * Рекурсия *//
+// let rec = 0;
+// function f1() {
+//     rec++;
+//     console.log(rec);
+//     // f1()
+// }
+// f1();
+
+let alcohol = ['Jack Daniels', 'Jameson', 'Egermaister'];
+alcohol.shift();
+console.log(alcohol[0]);
