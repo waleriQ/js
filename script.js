7bcffab30d4fcd0d507ad5a64738bb577e17c1e4
waleriQ <interceptor55@icloud.com>
2022-11-09 21:22:06 +0600

    method

--- script.js
+++ script.js
@@ -51,5 +51,14 @@
 
 //--------------
 
-let i = ['A', 'B', 'C', 'D', 'F'];
-console.log(i.length);
+function eachCons(array, n) {
+    const r = [];
+    for( let i = 0; i <= array.length - n; i++) {
+        const chunk = [];
+        for(let j = i; j < i+n; j++) {
+            chunk.push(array[j])
+        }
+        r.push[chunk];
+    }
+}
+eachCons([3, 5, 8, 13], 1);
